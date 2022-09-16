92936335028e536f6e1b89814004446723f1ac99
Niyaz <fexful@gmail.com>
2022-09-16 23:55:50 +0500

    First Commit

---
+++ DZ - 4.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ___4
+{
+    internal class Program
+    {
+        static String[] GetSplitString(String sentence)
+        {
+
+            String[] sentenceArray = sentence.Split(' ');
+            return sentenceArray;
+        }
+
+        static void WriteInConsoleSplitSentence(String[] splitSentence)
+        {
+            Console.WriteLine("Разделённое предложение: ");
+            foreach (String sentence in splitSentence)
+            {
+                System.Console.WriteLine($"{sentence} ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение для разделения: ");
+            WriteInConsoleSplitSentence(GetSplitString(Console.ReadLine()));
+
+
+            Console.ReadLine();
+        }
+    }
+}
